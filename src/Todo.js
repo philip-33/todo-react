dd3c224a3df25b5bf9a95c3086b47522994dfcc4
philip-33 <22357184+philip-33@users.noreply.github.com>
2022-05-16 00:11:59 -0700

    Add button, list display, uuid's, and checkboxes

--- src/Todo.js
+++ src/Todo.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Todo({ todo }) {
   return (
     <div>
+        <input type="checkbox" checked={todo.complete}></input>
         {todo.name}
     </div>
   )
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Todo from './Todo'
+import { v4 as uuidv4 } from 'uuid';
 
 export default function TodoList({ todos }) {
   return (
-    todos.map(todo => {
-        return <Todo key={todo} todo={todo} />
+    todos.map((todo, index) => {
+        return <Todo key={index} todo={todo} />
     })
   )
 }
