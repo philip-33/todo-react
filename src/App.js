eccecc0350e00841600039c68b99cf7566b323dc
philip-33 <22357184+philip-33@users.noreply.github.com>
2022-05-15 23:32:18 -0700

    Added to-do list skeleton functionality and components, customized templates + icons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState} from 'react';
 import TodoList from './TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([{ id: 1, name: "Todo 1", complete: false }])
   return (
     <>
-      <TodoList />
+      <TodoList todos={todos}/>
       <input type="text" />
       <button>Add Todo</button>
-      <button>Clear Completed Todos</button>
+      <button>Clear Complete</button>
       <div>0 left to do</div>
     </>
   )
